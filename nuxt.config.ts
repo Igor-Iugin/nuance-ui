473b9c5cd15224a02ffe13dbaba297e8a394e8ad
Igor Iugin <igoriugin@yandex.ru>
2025-11-24 15:38:44 +0300

    [init](config): Configured stylelint, postcss, nuxt-modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,51 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxt/eslint', '@nuxt/icon'],
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/icon',
+		'@nuxtjs/color-mode',
+	],
+	colorMode: {
+		dataValue: 'mantine-color-scheme',
+	},
+	icon: {
+		mode: 'svg',
+		class: '',
+		clientBundle: {
+			scan: {
+				globInclude: ['src/**/*.vue', 'src/**/*.ts'],
+				globExclude: [
+					'node_modules',
+					'.idea',
+					'.output',
+					'.data',
+					'.nuxt',
+					'.nitro',
+					'.cache',
+					'dist',
+					'server',
+				],
+			},
+			includeCustomCollections: true,
+			sizeLimitKb: 256,
+		},
+	},
+	eslint: {
+		config: { standalone: false },
+	},
+	postcss: {
+		plugins: {
+			'postcss-import': {},
+			'postcss-preset-mantine': {},
+			'postcss-simple-vars': {
+				variables: {
+					'breakpoint-xs': '36em',
+					'breakpoint-sm': '48em',
+					'breakpoint-md': '62em',
+					'breakpoint-lg': '75em',
+					'breakpoint-xl': '88em',
+				},
+			},
+			'autoprefixer': {},
+		},
+	},
 })
--- src/module.ts
+++ src/module.ts
@@ -1,20 +1,70 @@
-import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { addComponentsDir, addImportsDir, createResolver, defineNuxtModule } from '@nuxt/kit'
+import defu from 'defu'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {
+	/**
+	 * Prefix for component name
+	 * @default 'N'
+	 * @example 'N' => <NButton />
+	 */
+	prefix?: string
+
+	/**
+	 * Auto import components
+	 * @default true
+	 */
+	autoImport?: boolean
+
+	/**
+	 * Default theme
+	 * @default 'light'
+	 */
+	theme?: 'light' | 'dark' | 'auto'
 }
+
+const defaultConfig = {
+	autoImport: true,
+	theme: 'light',
+	prefix: 'N',
+} satisfies ModuleOptions
 
 export default defineNuxtModule<ModuleOptions>({
 	meta: {
-		name: 'my-module',
-		configKey: 'nuance',
+		name: '@nuxtjs/nuance-ui',
+		configKey: 'nui',
+		compatibility: {
+			nuxt: '^4.0.0',
+		},
+	},
+	moduleDependencies: {
+		'@nuxtjs/color-mode': {
+			version: '>=4.0.0',
+			overrides: {
+				dataValue: 'mantine-color-scheme',
+			},
+		},
 	},
 	// Default configuration options of the Nuxt module
-	defaults: {},
-	setup(_options, _nuxt) {
-		const resolver = createResolver(import.meta.url)
+	defaults: defaultConfig,
+	setup(options, nuxt) {
+		const { resolve } = createResolver(import.meta.url)
 
-		// Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-		addPlugin(resolver.resolve('./runtime/plugin'))
+		nuxt.options.appConfig.nui = defu(nuxt.options.appConfig.ui || {}, defaultConfig)
+
+		// Регистрируем компоненты с префиксом
+		if (options.autoImport) {
+			addComponentsDir({
+				path: resolve('./runtime/components'),
+				prefix: options.prefix,
+				pathPrefix: false,
+				ignore: ['color-mode/**'],
+			})
+
+			addImportsDir(resolve('./runtime/composables'))
+		}
+
+		// Add global styles
+		nuxt.options.css.push(resolve('./runtime/assets/css/main.css'))
 	},
 })
