2baa6f0fb369df4bebd25b0b66e6213b58680ced
Igor Iugin <igoriugin@yandex.ru>
2025-11-27 13:28:41 +0300

    chore: Add test for useTheme

--- playground/app.vue
+++ playground/app.vue
@@ -1,9 +1,24 @@
-<script setup lang='ts'>
+<script lang='ts' setup>
+const theme = useTheme()
 </script>
 
 <template>
-	<div>
-		Nuxt module playground!
-	</div>
+	<NAppShell aside footer navbar with-border>
+		<NAppShellHeader :class='$style.header'>
+			<NActionIcon @click='theme.preference = theme.value === "light" ? "dark" : "light"'>
+				<Icon :name='theme.value === "light" ? "gravity-ui:moon" : "gravity-ui:sun"' />
+			</NActionIcon>
+		</NAppShellHeader>
+		<NAppShellMain>
+			<slot />
+		</NAppShellMain>
+	</NAppShell>
 </template>
 
+<style lang='postcss' module>
+.header {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+</style>
