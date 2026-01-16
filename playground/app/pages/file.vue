17c3d94a8b46fb161753f4b92bc49b7b29229784
Igor Iugin <igoriugin@yandex.ru>
2026-01-16 16:05:17 +0300

    feat(new): FileUpload button

---
+++ playground/app/pages/file.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const cFile = shallowRef<File | null>(null)
+const files = shallowRef<File[] | null>(null)
+</script>
+
+<template>
+	<NTitle>Components for management files</NTitle>
+	<NFileUpload @change='f => cFile = f' />
+	<NText v-if='cFile'>
+		{{ cFile.name }}
+	</NText>
+
+	<hr>
+
+	<NFileUpload multiple @change='f => files = f' />
+	<NText v-for='file in files' :key='file.name'>
+		{{ file.name }}
+	</NText>
+</template>
