62d1816e35b5544c3fc3a420f53d7565936186fe
Igor Iugin <igoriugin@yandex.ru>
2026-02-09 12:09:25 +0300

    fix: :art: Fix import order

--- src/runtime/components/select/select.vue
+++ src/runtime/components/select/select.vue
@@ -8,8 +8,8 @@ import { useCombobox, useComboboxData } from '../combobox'
 import ComboboxOptionsDropdown from '../combobox/combobox-options-dropdown.vue'
 import ComboboxRoot from '../combobox/combobox-root.vue'
 import ComboboxTarget from '../combobox/combobox-target.vue'
-import ButtonInput from '../input/ui/button-input.vue'
 import TextInput from '../input/text-input.vue'
+import ButtonInput from '../input/ui/button-input.vue'
 
 
 export interface SelectProps<
