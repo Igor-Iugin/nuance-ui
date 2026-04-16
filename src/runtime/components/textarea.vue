21d8040efc7003dcf503db0b935b150f2e2710dc
Igor Iugin <igoriugin@yandex.ru>
2026-04-16 17:01:07 +0300

    feat: :sparkles: Created autosize logic in textarea

--- src/runtime/components/textarea.vue
+++ src/runtime/components/textarea.vue
@@ -1,9 +1,9 @@
 <script setup lang='ts'>
-import { unrefElement, useTextareaAutosize } from '@vueuse/core'
 import { useTemplateRef } from 'vue'
 
 import type { InputBaseProps, InputWrapperProps } from './input'
 
+import { useAutosize } from '../composables/use-autosize'
 import InputBase from './input/ui/input-base.vue'
 import InputWrapper from './input/ui/input-wrapper.vue'
 
@@ -11,27 +11,43 @@ import InputWrapper from './input/ui/input-wrapper.vue'
 export interface TextareaProps extends InputWrapperProps, InputBaseProps {
 	/** Icon displayed in the left section */
 	icon?: string
+
+	/** If set, enables textarea height growing with its content @default false */
+	autosize?: boolean
+
+	/** Maximum rows for autosize textarea to grow, ignored if `autosize` prop is not set */
+	maxRows?: number
+
+	/** Minimum rows of autosize textarea, ignored if `autosize` prop is not set */
+	minRows?: number
 }
 
-const { icon, ...props } = defineProps<TextareaProps>()
+const {
+	icon,
+	autosize = false,
+	minRows,
+	maxRows,
+	...props
+} = defineProps<TextareaProps>()
 
 const model = defineModel<string>()
+const inputRef = useTemplateRef<HTMLTextAreaElement>('input')
 
-const ref = useTemplateRef<HTMLTextAreaElement>('input')
-const { input } = useTextareaAutosize({
-	input: model.value,
-	styleProp: 'minHeight',
-	element: unrefElement(ref.value),
+useAutosize(inputRef, {
+	model,
+	minRows,
+	maxRows,
+	enabled: autosize,
 })
 
 defineExpose({
-	$el: unrefElement(ref.value),
+	$el: inputRef,
 })
 </script>
 
 <template>
 	<InputWrapper v-bind='props' :multiline :class='$attrs?.class'>
-		<InputBase>
+		<InputBase :classes='{ root: $style.test }'>
 			<template v-if='!!$slots.leftSection || icon' #leftSection>
 				<slot name='leftSection'>
 					<Icon v-if='icon' :name='icon' />
@@ -46,7 +62,7 @@ defineExpose({
 					v-model='model'
 					:class='[css, $style.textarea]'
 					:required='props.required'
-					:disabled='props.required'
+					:disabled='props.disabled'
 					:readonly='props.readonly'
 				/>
 			</template>
