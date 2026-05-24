76d12fc652df7dc5c65a75078602fb38ba008364
Igor-Iugin <igoriugin@yandex.ru>
2026-05-24 14:34:54 +0300

    feat: :sparkles: new Center component

---
+++ src/runtime/components/center.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import type { BoxProps } from './box.vue'
+
+import Box from './box.vue'
+
+
+export interface CenterProps extends BoxProps {
+	/** If set, `inline-flex` is used instead of `flex` */
+	inline?: boolean
+}
+const {
+	inline,
+	mod,
+	...props
+} = defineProps<CenterProps>()
+</script>
+
+<template>
+	<Box
+		v-bind='props'
+		:class='$style.root'
+		:mod='[{ inline }, mod]'
+	>
+		<slot />
+	</Box>
+</template>
+
+<style module>
+.root {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	&:where([data-inline]) {
+		display: inline-flex;
+	}
+}
+</style>
