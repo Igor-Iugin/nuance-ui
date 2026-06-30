32ce423815a0728692b391274db086c87a837978
Igor-Iugin <igoriugin@yandex.ru>
2026-07-01 00:25:09 +0300

    chore: Setup test env

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,31 @@
+import { defineVitestProject } from '@nuxt/test-utils/config'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
 
 
 export default defineConfig({
 	test: {
-		environment: 'happy-dom',
-		pool: 'forks',
+		projects: [
+			{
+				test: {
+					name: 'unit',
+					include: ['src/runtime/**/*.{test,spec}.ts'],
+					environment: 'node',
+				},
+			},
+			await defineVitestProject({
+				test: {
+					name: 'nuxt',
+					include: ['test/**/*.{test,spec}.ts'],
+					environment: 'nuxt',
+					environmentOptions: {
+						nuxt: {
+							rootDir: fileURLToPath(new URL('./playground', import.meta.url)),
+							domEnvironment: 'happy-dom',
+						},
+					},
+				},
+			}),
+		],
 	},
 })
