1eacf31eb5011eb18b0cdbf106ae82ef531f377a
Igor-Iugin <igoriugin@yandex.ru>
2026-07-01 10:50:28 +0300

    chore(playground): Added optimize packages

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,3 +1,14 @@
 export default defineNuxtConfig({
 	modules: ['../src/module'],
+	vite: {
+		optimizeDeps: {
+			include: [
+				'@vue/devtools-core',
+				'@vue/devtools-kit',
+				'@vueuse/core',
+				'@formkit/tempo',
+				'es-toolkit',
+			],
+		},
+	},
 })
