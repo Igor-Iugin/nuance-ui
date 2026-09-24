79701fa4eae9b69a46c982d26315316c6816fccf
Igor Yugin <i.yugin@robo-ts.ru>
2026-09-24 13:06:34 +0300

    feat(button): Added ButtonSection addon component

---
+++ src/runtime/components/button/button-section.vue
@@ -0,0 +1,85 @@
+<script setup lang='ts'>
+import type { AnyString, NuanceColor, NuanceGradient, NuanceRadius, NuanceSize } from '@nui/types'
+
+import { useConfig, useVarsResolver } from '@nui/composables'
+import { BUTTON_SIZE_TOKENS, getFontSize, getRadius, getSize } from '@nui/utils'
+
+import type { BoxProps } from '../box/box.vue'
+import type { ButtonVariant } from './button.vue'
+
+import Box from '../box/box.vue'
+import css from './button.module.css'
+
+
+export interface ButtonSectionVars {
+	root:
+		| '--addon-height'
+		| '--addon-padding-x'
+		| '--addon-fz'
+		| '--addon-radius'
+		| '--addon-bg'
+		| '--addon-color'
+		| '--addon-bd'
+}
+
+export interface ButtonSectionProps extends BoxProps {
+	/** Component size */
+	size?: NuanceSize | `compact-${NuanceSize}` | `input-${NuanceSize}` | AnyString
+
+	/** Gradient configuration (used with `variant="gradient"`) */
+	gradient?: NuanceGradient
+
+	/** Color from theme */
+	color?: NuanceColor
+
+	/** Border radius */
+	radius?: NuanceRadius | AnyString
+
+	/** Visual variant */
+	variant?: ButtonVariant
+}
+
+const {
+	is,
+	mod,
+	gradient,
+	radius,
+	size,
+	variant = 'default',
+	color,
+} = defineProps<ButtonSectionProps>()
+
+const { variantResolver } = useConfig()
+
+const style = useVarsResolver<ButtonSectionVars>(theme => {
+	const { background, border, text } = variantResolver({
+		variant,
+		color,
+		theme,
+		gradient,
+	})
+
+	return {
+		root: {
+			'--addon-height': getSize(size, 'addon-height', BUTTON_SIZE_TOKENS),
+			'--addon-padding-x': getSize(size, 'addon-padding-x', BUTTON_SIZE_TOKENS),
+			'--addon-fz': getFontSize(size),
+			'--addon-radius': radius === undefined ? undefined : getRadius(radius),
+			'--addon-bg': background,
+			'--addon-color': text,
+			'--addon-bd': border,
+		},
+	}
+})
+</script>
+
+<template>
+	<Box
+		:is
+		:mod='[{ variant }, mod]'
+		:style='style.root'
+		:class='css.addon'
+	>
+		<slot />
+	</Box>
+</template>
--- src/runtime/components/button/index.ts
+++ src/runtime/components/button/index.ts
@@ -1,3 +1,4 @@
 export type * from './button-group.vue'
+export type * from './button-section.vue'
 export type * from './button.vue'
 export type * from './unstyled-button.vue'
